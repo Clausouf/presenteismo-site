ee0c90f85c5809d7f12bf7c56a38693e8fa3363b
Clausouf <clauber.sfernandes@gmail.com>
2026-07-09 14:05:14 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+// Se por acaso as variáveis sumirem no build, ele usa uma URL falsa temporária para não quebrar o processo
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://hevrxoccodxlxrxwdpwj.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_ByNB1KIkqdsy_LeoWAkkIA_di9Q1lpz';
 
 export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
