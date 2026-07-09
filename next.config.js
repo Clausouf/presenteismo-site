25850ea2b827679004d365fb5bee73a0dda6efee
Clausouf <clauber.sfernandes@gmail.com>
2026-07-09 14:17:28 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     // Ignora erros do TypeScript no build para liberar o deploy
     ignoreBuildErrors: true,
