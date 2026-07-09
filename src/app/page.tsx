6e08ba561144efedb7b1d83a5dedbe353d2381a4
Clausouf <clauber.sfernandes@gmail.com>
2026-07-09 12:21:10 -0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation';
+
+export default function RootPage() {
+  // Redireciona automaticamente a raiz do site para o painel de controle seguro
+  redirect('/dashboard');
+}
