6d078628de1990327e5662387f1160b23b87a02c
Clausouf <clauber.sfernandes@gmail.com>
2026-07-21 10:47:16 -0300

    Create page.tsx

---
+++ src/app/dashboard/consolidado/page.tsx
@@ -0,0 +1,5 @@
+import DashboardBase from '@/components/DashboardBase'; // ou o caminho correto onde está o componente base
+
+export default function ConsolidadoPage() {
+  return <DashboardBase tipo="consolidado" />;
+}
